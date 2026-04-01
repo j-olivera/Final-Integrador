205adca96f5ca090d57c9df45f7983d1db9892df
ju4n_0 <juanswc87@gmail.com>
2026-04-01 19:18:40 -0300

    config para conexion con el front

--- src/main/java/com/olivera/challenge/infrastructure/controllers/order/OrderController.java
+++ src/main/java/com/olivera/challenge/infrastructure/controllers/order/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin("http://localhost:4200")
 public class OrderController {
 
     private final CreateOrder createOrder;
--- src/main/java/com/olivera/challenge/infrastructure/controllers/user/UserController.java
+++ src/main/java/com/olivera/challenge/infrastructure/controllers/user/UserController.java
@@ -5,13 +5,11 @@ import com.olivera.challenge.application.dto.response.UserResponse;
 import com.olivera.challenge.application.port.in.user.RegisterUser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin("http://localhost:4200")
 public class UserController {
     private final RegisterUser registerUser;
 
